78cf8551802390f10ebf97317804242d60216373
PoolloverNathan <nathan.kulzer@protonmail.com>
2024-06-26 15:21:23 -0400

    return of the shit

--- user/nathan.nix
+++ user/nathan.nix
@@ -8,39 +8,39 @@
   system.hashedPassword = "$y$j9T$lfDMkzctZ7jVUA.rK6U/3/$stLjTnRqME75oum.040Ya7tKAPsnIJ.gAZYQk57vNp2";
   system.userDescription = "PoolloverNathan";
   home.stateVersion = "24.11";
-  # home.packages = builtins.attrValues rec {
-  #   inherit (pkgs)
-  #   blockbench
-  #   clinfo
-  #   ed
-  #   emacs
-  #   firefox
-  #   fprintd
-  #   gh
-  #   ghc
-  #   glxinfo
-  #   htop
-  #   jdk17
-  #   # kde-connect
-  #   kitty
-  #   neovim
-  #   prismlauncher
-  #   python312Full
-  #   thefuck
-  #   vscodium
-  #   xclip
-  #   xsel;
-  #   # inherit (pkgs.jetbrains)
-  #   # idea-community;
-  #   discord = pkgs.discord.override {
-  #     withOpenASAR = true;
-  #     withVencord = true;
-  #     inherit vencord;
-  #   };
-  #   vencord = (import "${sadan4}/customPackages" { inherit pkgs; }).vencord.overrideAttrs {
-  #     # patches = [./vencord-no-required.patch];
-  #     # patchFlags = ["-p0"];
-  #   };
-  #   fok-quote = fokquote.packages.${pkgs.system}.default;
-  # };
+  home.packages = builtins.attrValues rec {
+    inherit (pkgs)
+    blockbench
+    clinfo
+    ed
+    emacs
+    firefox
+    fprintd
+    gh
+    ghc
+    glxinfo
+    htop
+    jdk17
+    # kde-connect
+    kitty
+    neovim
+    prismlauncher
+    python312Full
+    thefuck
+    vscodium
+    xclip
+    xsel;
+    # inherit (pkgs.jetbrains)
+    # idea-community;
+    discord = pkgs.discord.override {
+      withOpenASAR = true;
+      withVencord = true;
+      inherit vencord;
+    };
+    vencord = (import "${sadan4}/customPackages" { inherit pkgs; }).vencord.overrideAttrs {
+      # patches = [./vencord-no-required.patch];
+      # patchFlags = ["-p0"];
+    };
+    fok-quote = fokquote.packages.${pkgs.system}.default;
+  };
 }
