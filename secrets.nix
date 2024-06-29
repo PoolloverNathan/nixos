934a1603b896e175d1854bf5d9e9009bfa449501
PoolloverNathan <nathan.kulzer@protonmail.com>
2024-06-29 00:42:18 -0400

    bunny password

--- secrets.nix
+++ secrets.nix
@@ -1,4 +1,4 @@
 {
   users.users.root.hashedPassword = "$y$j9T$iK52G8r4zzkHpKatW5h7M/$3UI9Q1KCPAd6hUDGfe.vdUveQaZ8jWDdV2U9CFIsfk1";
-  users.users.bunny.hashedPassword = "$6$DBzGDvhsdb1HKl03$eWknKeBiYviFgaljKHTFJ3qSK6mupjQSP9Kib9j.wLVD0hd6.t4ikEbmTvC7KHv/VEx.HolqFCfvyPhm1LDv10";
+  users.users.bunny.hashedPassword = "$y$j9T$anfc1IsFmtwULD0XyqK0I/$XTVKEsexMmM32qW2yZWEFXtsLqRv15mCtKEhtHBDhq8";
 }
