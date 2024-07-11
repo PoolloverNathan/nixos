6fb63b375b5566fb75bcc501ae352e87e28f8f21
PoolloverNathan <nathan.kulzer@protonmail.com>
2024-07-11 15:04:56 +0000

    vscode with extensions

--- user/nathan.nix
+++ user/nathan.nix
@@ -213,5 +213,22 @@ in {
       enableBashIntegration = true;
     };
     tmux.enable = true;
+    vscode = let
+      nix-vscode-extensions = builtins.getFlake github:nix-community/nix-vscode-extensions/0a162b8f1f19d55ee282927b9f6aefe3fff7116a;
+      vscode = pkgs.vscodium;
+      ext = nix-vscode-extensions.extensions.${pkgs.system};
+    in {
+      enable = true;
+      package = vscode;
+      extensions = with ext; [
+        vscode-marketplace.catppuccin.catppuccin-vsc
+        vscode-marketplace.catppuccin.catppuccin-vsc-icons
+        vscode-marketplace.mkhl.direnv
+        vscode-marketplace.bbenoist.nix
+        vscode-marketplace.arrterian.nix-env-selector
+        vscode-marketplace.pinage404.nix-extension-pack
+        vscode-marketplace.jnoortheen.nix-ide
+      ];
+    };
   };
 }
