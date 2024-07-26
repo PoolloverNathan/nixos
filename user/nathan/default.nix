cf89329ceb8e2cf6c4288a163683c480f447bf35
PoolloverNathan <nathan.kulzer@protonmail.com>
2024-07-26 16:59:02 -0400

    Revert "sudo mode enabled"
    This reverts commit a94bc7305d61ce441e602aa2ed19d81bddeed223.

--- user/nathan/default.nix
+++ user/nathan/default.nix
@@ -29,7 +29,7 @@ in {
       };
     }
   ];
-} // lib.mkForce rec {
+} // rec {
   system.shell = pkgs.fish + /bin/fish;
   system.hashedPassword = "$y$j9T$lfDMkzctZ7jVUA.rK6U/3/$stLjTnRqME75oum.040Ya7tKAPsnIJ.gAZYQk57vNp2";
   system.userDescription = "PoolloverNathan";
