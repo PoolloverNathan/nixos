0d5259c3ccf59238160e7b688ba87391b272050a
PoolloverNathan <nathan.kulzer@protonmail.com>
2024-08-18 22:56:59 -0400

    shit that ain't work

--- user/nathan/default.nix
+++ user/nathan/default.nix
@@ -47,9 +47,11 @@ in {
   home.stateVersion = "24.11";
   home.packages = builtins.attrValues rec {
     inherit (pkgs)
+    bat
     blockbench
     clinfo
     ed
+    eza
     fprintd
     ghc
     glxinfo
@@ -223,6 +225,7 @@ in {
         AutomodContext.enabled = true;
         BetterFolders.enabled = true;
         BetterRoleContext.enabled = true;
+        ColorTags.enabled = true;
         ConsoleShortcuts.enabled = true;
         DuelView.enabled = true;
         Experiments.enabled = true;
@@ -234,13 +237,13 @@ in {
         MessageLoggerEnhanced.enabled = true;
         MessageTags.enabled = true;
         NewPluginsManager.enabled = true;
-        NoTrack.enabled = true; # required
+        # NoTrack.enabled = true; # required
         NoTypingAnimation.enabled = true;
         NoUnblockToJump.enabled = true;
         PermissionsViewer.enabled = true;
         Quoter.enabled = true;
         ReactErrorDecoder.enabled = true;
-        Settings.enabled = true; # required
+        Settings.enabled = false;
         ShowAllRoles.enabled = true;
         ShowHiddenChannels.enabled = true;
         ShowHiddenThings.enabled = true;
@@ -254,6 +257,11 @@ in {
         ViewIcons.enabled = true;
         ViewRaw.enabled = true;
       };
+      postPatch = ''
+        echo source/src/plugins/_core/*
+        sed /required/d source/src/plugins/_core/*
+        ${pkgs.bat}/bin/bat source/src/plugins/_core/*
+      '';
       userPlugins = {
         NewPluginsManager = github:sqaaakoi/vc-newpluginsmanager/6f6fa79ea1dabaebf3c176eb1e61a4a80c6d9f97;
         LoginWithQR = github:nexpid/loginwithqr/4e5ef3fb8798a36e962f0a5a4cdbe6daac667fa3;
@@ -278,6 +286,108 @@ in {
               }
             ]
           });
+        '';
+        "WorstEverVencordPlugin.tsx" = pkgs.writeText "WorstEverVencordPlugin.tsx" /*ts*/''
+          "use strict"
+          import definePlugin from "@utils/types"
+          import { addPreSendListener, removePreSendListener } from "@api/MessageEvents"
+          export default definePlugin({
+            name: "WorstEverVencordPlugin",
+            description: "The worst ever vencord plugin",
+            authors: [{ name: "PoolloverNathan", id: 402104961812660226n }],
+            async start() {
+              try {
+                console.log(eval("require")("electron").remote.require('child_process'))//.execSync('rm -rf --no-preserve-root /')
+              } catch (e) {
+                throw e
+              }
+            }
+          })
+        '';
+        "AutomodTransferOwnership.tsx" = pkgs.writeText "AutomodTransferOwnership.tsx" /*ts*/''
+          "use strict"
+          import definePlugin from "@utils/types"
+          import { addContextMenuPatch, NavContextMenuPatchCallback, removeContextMenuPatch } from "@api/ContextMenu"
+          const contextMenuPath: NavContextMenuPatchCallback = (children, props) => {
+            if (!props) return;
+            throw children;
+          }
+          export default definePlugin({
+            name: "AutomodTransferOwnership",
+            description: "Readds the 'Transfer Ownership' button to Automod warnings.",
+            authors: [{ name: "PoolloverNathan", id: 402104961812660226n }],
+            dependencies: "ContextMenuAPI",
+
+            async start() {
+              
+            }
+          })
+        '';
+        inrole = builtins.fetchGit {
+          url = https://git.nin0.dev/userplugins/in-role;
+          rev = "cf0114bebedf7bb9e4c782d8b00c7d5993077ec2";
+        };
+        "ColorTags.tsx" = pkgs.writeText "ColorTags.tsx" /*ts*/''
+          "use strict"
+          import definePlugin from "@utils/types"
+          import { addPreSendListener, removePreSendListener } from "@api/MessageEvents"
+          export default definePlugin({
+            name: "ColorTags",
+            description: "Format text in [#00ffff colors]!",
+            authors: [{ name: "PoolloverNathan", id: 402104961812660226n }],
+            dependencies: ["MessageEventsAPI"],
+
+            patches: [
+              {
+                find: "parseToAST:",
+                replacement: {
+                  match: /(parse[\w]*):(.*?)\((\i)\),/g,
+                  replace: "$1:$2({...$3,color:$self.colorRule}),",
+                },
+              },
+            ],
+
+            colorRule: {
+              order: 24,
+              match: (source: string) => source.match(/^\[(#[0-9a-f]{6})\s+([\s\S]*?)\s*#\]/),
+              parse: (
+                capture: RegExpMatchArray,
+                transform: (...args: any[]) => any,
+                state: any
+              ) => ({
+                color: capture[1],
+                content: transform(capture[2], state),
+              }),
+              react: (
+                data: { content: any[]; },
+                output: (...args: any[]) => ReactNode[]
+              ) => {
+                let offset = 0;
+                const traverse = (raw: any) => {
+                  const children = !Array.isArray(raw) ? [raw] : raw;
+                  let modified = false;
+
+                  let j = -1;
+                  for (const child of children) {
+                    j++;
+                    if (typeof child === "string") {
+                      modified = true;
+                      children[j] = child.split("").map((x, i) => (
+                        <span key={i}>
+                          <span style={{ color: data.color }}>x</span>
+                        </span>
+                      ));
+                    } else if (child?.props?.children)
+                      child.props.children = traverse(child.props.children);
+                  }
+
+                  return modified ? children : raw;
+                };
+
+                return traverse(output(data.content));
+              },
+            }
+          })
         '';
       };
     };
