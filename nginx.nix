22a7fe185a8559476e589db0f7a983b295d9a431
PoolloverNathan <nathan.kulzer@protonmail.com>
2024-11-01 14:22:46 -0400

    style: et nginx.nix

--- nginx.nix
+++ nginx.nix
@@ -1,14 +1,14 @@
 { ... }: {
-	imports = [
-		./binary-cache.nix
-	];
-	services.nginx = {
-		enable = true;
-		recommendedProxySettings = true;
-		recommendedTlsSettings = true;
-	};
-	security.acme = {
-		acceptTerms = true;
-		defaults.email = "nathan.kulzer+acme@protonmail.com";
-	};
+  imports = [
+    ./binary-cache.nix
+  ];
+  services.nginx = {
+    enable = true;
+    recommendedProxySettings = true;
+    recommendedTlsSettings = true;
+  };
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = "nathan.kulzer+acme@protonmail.com";
+  };
 }
