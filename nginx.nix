756ee0e3782c524c7428f03658a5ca277a5128ea
PoolloverNathan <nathan.kulzer@protonmail.com>
2024-11-03 16:17:37 -0500

    feat: sculptor vhost
    currently requires hosting sculptor manually in a tty

--- nginx.nix
+++ nginx.nix
@@ -6,6 +6,13 @@
     enable = true;
     recommendedProxySettings = true;
     recommendedTlsSettings = true;
+    virtualHosts = {
+      "figura.pool.net.eu.org" = {
+        addSSL = true;
+        enableACME = true;
+        locations."/".proxyPass = "http://localhost:6665";
+      };
+    };
   };
   security.acme = {
     acceptTerms = true;
