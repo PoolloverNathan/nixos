1bc5fb60e5ac285ce1c26bf0fed7921b2d29f850
PoolloverNathan <nathan.kulzer@protonmail.com>
2024-11-07 11:28:55 -0500

    no catppuccin

--- user/nathan/default.nix
+++ user/nathan/default.nix
@@ -280,10 +280,6 @@ in {
     vencord = {
       enable = true;
       themes = {
-        catppuccin = pkgs.fetchurl {
-          url = "https://catppuccin.github.io/discord/dist/catppuccin-${catppuccin.flavor}-${catppuccin.accent}.theme.css";
-          hash = sha256:uaYo7x0YHw0dJlzP6loIiQFxCU4HPvAUwiqQnaTZxn4=;
-        };
         square-corners = pkgs.fetchurl {
           url = https://gist.githubusercontent.com/TheBunnyMan123/6315b2b6db6096ae8485736b4ebbceff/raw/14356b1435db17afcdd7f5d50831b499abc7b4c8/squarecorners.theme.css;
           hash = sha256:AdiIfmq0Vc6VlmlanvIUnkNlvOjLciwdpZxtAYYXtCQ=;
