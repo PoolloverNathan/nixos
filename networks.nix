9134a85a08ab47630a95ac4054a15d646fd244c2
PoolloverNathan <nathan.kulzer@protonmail.com>
2024-12-25 16:28:47 -0500

    fix: new network

--- networks.nix
+++ networks.nix
@@ -1,5 +1,6 @@
 {
   NETGEAR25.psk = "S6PRPWPTXFMC35M5";
+  Kulzer.psk = "S6PRPWPTXFMC35M5";
   XB7C8.psk = "JW5DJ9SRZ68QPJKB";
   WhyWontItConnect.psk = "46e074db90b1";
   DIRECT-TF-USER-NathanPhone.psk = "6hfXjDul";
